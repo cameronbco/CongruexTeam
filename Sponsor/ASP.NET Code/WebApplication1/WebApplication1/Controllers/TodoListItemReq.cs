6384a6f5e145e9627cc5bdc04f1bfa570590a3bb
cameron-brown2 <cabr3402@colorado.edu>
2022-11-09 01:27:39 -0700

    Finally working
    Solved issue where code needed to be run as web application.

---
+++ Sponsor/ASP.NET Code/WebApplication1/WebApplication1/Controllers/TodoListItemReq.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using To_Do_Widget.DTO;
+
+namespace To_Do_Widget.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class TodoListItemController : ControllerBase
+    {
+        public TodoListItemController()
+        {
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoListItemRequest>>> Get()
+        {
+            // Return NotFound() == 404
+            // Return NoContent() and so on.
+            return FakeData.GetTodoListItems();
+        }
+        [HttpPost]
+        public async Task<ActionResult<TodoListItemRequest>> PostTodoListItem(TodoListItemRequest request)
+        {
+            TodoListItemRequest item = FakeData.CreateTodoListItemRequest(request.Name, request.IsComplete);
+            return item;
+        }
+    }
+}
---
+++ Sponsor/ASP.NET Code/WebApplication1/WebApplication1/DTO/FakeData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace To_Do_Widget.DTO
+{
+    public class FakeData
+    {
+               private static List<TodoListItemRequest> _requests { get; set; }
+
+
+
+       public static int _nextRequestId = 0;
+
+
+
+       public static List<TodoListItemRequest> GetTodoListItems()
+        {
+            if (_requests == null)
+            {
+                _requests = new List<TodoListItemRequest>();
+
+
+
+               _requests.Add(new TodoListItemRequest()
+                {
+                    Id = GetNextId(),
+                    IsComplete = false,
+                    Name = "Do Demo of c# Web API"
+                });
+
+
+
+           }
+
+
+
+           return _requests;
+        }
+
+
+
+       public static TodoListItemRequest CreateTodoListItemRequest(string name, bool isComplete)
+        {
+            var request = new TodoListItemRequest()
+            {
+                Id = GetNextId(),
+                IsComplete = isComplete,
+                Name = name
+            };
+
+
+
+           _requests.Add(request);
+
+
+
+           return request;
+        }
+
+
+
+       private static long GetNextId()
+        {
+            _nextRequestId = _nextRequestId + 1;
+
+
+
+           return _nextRequestId;
+        }
+    }
+}
