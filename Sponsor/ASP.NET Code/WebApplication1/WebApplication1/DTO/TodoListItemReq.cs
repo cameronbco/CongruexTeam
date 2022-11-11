68316ad52dd10de3b320b4ee81f1a79effa19002
avacca1 <anva5757@colorado.edu>
2022-11-11 10:29:35 -0700

    Change Return Data

--- Sponsor/ASP.NET Code/WebApplication1/WebApplication1/DTO/TodoListItemReq.cs
+++ Sponsor/ASP.NET Code/WebApplication1/WebApplication1/DTO/TodoListItemReq.cs
@@ -6,9 +6,18 @@ namespace To_Do_Widget.DTO
 {
     public class TodoListItemRequest
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
+        //public long Id { get; set; }
+        //public string Name { get; set; }
 
+
+        public string title { get; set; }
+        public List<child> childrens { get; set; }
         public bool IsComplete { get; set; }
+    }
+
+    public class child
+    {
+        public string title { get; set; }
+        public string path { get; set; }
     }
 }
