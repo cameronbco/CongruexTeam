66782004454e5cd371a3d741cba74a5f13ad194a
TheOCDTowel <cheng.lin@colorado.edu>
2022-11-17 18:49:27 -0700

    updated navbar and todo

--- Sponsor/congruex_site/src/hooks/index.js
+++ Sponsor/congruex_site/src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useLocalStorage } from './useLocalStorage/useLocalStorage';
 export { default as UseComponentVisible } from './useComponentVisible/useComponentVisible';
 export { default as UseDropdownData } from './UseDropdownData/UseDropdownData';
+export { default as useOnClickOutside } from './useOnClickOutside/useOnClickOutside';
--- Sponsor/congruex_site/src/widgets/TodoDropdown/TodoDropdownItem.js
+++ Sponsor/congruex_site/src/widgets/TodoDropdown/TodoDropdownItem.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import { Divider } from '@mui/material';
 
-export function TodoItems({item}) {
+function TodoItems({item, itemNum}) {
     return (
         <>
             <div><span className='font-semibold'>Title: </span>{item.title}</div>
             <div><span className='font-semibold'>Description: </span>{item.description}</div>
-            <div><span className='font-semibold'>Status: </span>{item.isComplete ? "Completed" : "Not Completed"}</div>
+            <div><span className='font-semibold'>Status: </span>{item.isComplete === false ? <CheckBoxOutlineBlankIcon /> : <CheckBoxIcon />}</div>
+            {/* add divider if element is not last */}
+            {item.id + 1 !== itemNum ? <Divider /> : null}
         </>
     )
 }
@@ -19,14 +24,16 @@ export default function TodoDropdownItem({item}) {
 
     return (
         <div>
-            <div className='task_section' onClick={handleClick}>
-                <div className='flex space-x-2 font-bold text-2xl svg-bold p-1 inline-flex items-center rounded-md hover:bg-gray-300 hover:cursor-pointer'> 
+            <div className='task_section'>
+                <div onClick={handleClick} className='w-ful space-x-2 font-bold text-2xl inline-flex p-1 content-center items-center rounded-md hover:bg-gray-300 hover:cursor-pointer'> 
                     <div>{item.name}</div>
                     <div>{open ? <AiOutlineUp /> : <AiOutlineDown />}</div>
                 </div>
                 {open && (
                     <div className='body'>
-                        {item.todoListItems.map((item, id) => <TodoItems key={id} item={item} />)}
+                        {item.todoListItems.map((items, id) => 
+                            <TodoItems key={id} item={items} itemNum={item.todoListItems.length}/>
+                        )}
                     </div>
                 )}
             </div>
